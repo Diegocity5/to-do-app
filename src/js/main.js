45f0c886ce0cddba8b9447b3c8813db8fbecf5ea
Diegocity5 <icotecc@gmail.com>
2025-03-04 00:35:56 -0500

    5: Agregando un array vacio que se encargara de guardar mis tareas y un objeto vacio para rastrear el estado de la tarea actual

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,5 @@ const confirmCloseDialog = document.getElementById('confirm-close-dialog');
 const cancelBtn = document.getElementById('cancel-btn');
 const discardBtn = document.getElementById('discard-btn');
 const tasksContainer = document.getElementById('task-container');
+const taskData = [];
+const currentTask = {};
